1b3e8b9a1b343a2593ebff734c45128cf569d984
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 10:54:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question no # 5          Famous Quote 2:
+let famous_person = 'Ali Ibn Abi Talib';
+let message = '"The disease of the heart is worse than the disease of the body."';
+console.log(`${famous_person}, ${message}`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Question no # 5          Famous Quote 2:
+
+let famous_person: string = 'Ali Ibn Abi Talib';
+let message: string = '"The disease of the heart is worse than the disease of the body."';
+console.log(`${famous_person}, ${message}`);
